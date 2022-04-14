3021e09fb4328410130b1cc48b1806b9645ade35
luis sandoval <sandovalhorna125@hotmail.com>
2022-04-14 01:47:35 -0300

    implementacion de struct Fecha

--- exerciseOne/TipoDeDatoFecha.h
+++ exerciseOne/TipoDeDatoFecha.h
@@ -1,3 +1,25 @@
 #pragma once
 enum class Dia {LUNES=1,MARTES=2,MIERCOLES=3,JUEVES=4,VIERNES=5,SABADO=6,DOMINGO=7};
 enum class Mes{ENERO=1,FEBRERO=2,MARZO=3,ABRIL=4,MAYO=5,JUNIO=6,JULIO=7,AGOSTO=8,SEPTIEMBRE=9,OCTUBRE=10,NOVIEMBRE=11,DICIEMBRE=12};
+struct Fecha
+{
+private:
+	int DiaNumero;
+	Dia DiaNombre;
+	Mes MesDelAnio;
+	int anio;
+public:
+	Fecha(int dia, Dia _dia, Mes _mes, int anio);
+	~Fecha();
+	int getDiaNumero()const;
+	string getDiaNombre()const;
+	string getMes()const;
+	int getAnio()const;
+
+	void setDiaNumero(const int& dia);
+	void setDiaNombre(const Dia& dia);
+	void setMes(const Mes& mes);
+	void setAnio(const int& anio);
+	string getData();
+
+};
